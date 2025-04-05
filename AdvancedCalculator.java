de9156d8fd1a9f7b2ced050b6b28816bd4257834
blanksachin <shresthasachin673@gmail.com>
2025-04-05 01:38:55 -0400

    Added a comments on the methods

--- AdvancedCalculator.java
+++ AdvancedCalculator.java
@@ -1,17 +1,18 @@
 public class AdvancedCalculator {
-
+// Throws exception for 0^-n
     public static double power(double base, int exponent) {
         if (base == 0 && exponent < 0)
             throw new ArithmeticException("Cannot raise 0 to a negative power.");
         return Math.pow(base, exponent);
     }
-
+// Requires non-negative input
     public static double squareRoot(double num) {
         if (num < 0)
             throw new IllegalArgumentException("Cannot take square root of negative number.");
         return Math.sqrt(num);
     }
 
+ // Returns false for numbers <= 1
     public static boolean isPrime(int num) {
         if (num <= 1)
             return false;
@@ -21,6 +22,8 @@ public class AdvancedCalculator {
         return true;
     }
 
+
+    // Returns 1 for 0, throws for negatives
     public static long factorial(int n) {
         if (n < 0)
             throw new IllegalArgumentException("Factorial is undefined for negative numbers.");
