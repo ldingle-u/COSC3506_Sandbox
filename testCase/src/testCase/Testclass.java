2c8e4815396ef6c290b194a0b3ea4fda02875014
CoderJoe <162841361+TheCodester12@users.noreply.github.com>
2025-11-29 11:45:58 -0500

    Created Separate Class

--- testCase/src/testCase/Testclass.java
+++ testCase/src/testCase/Testclass.java
@@ -1,39 +1,10 @@
 package testCase;
 
 public class Testclass {
-
-    public static double power(double base, int exponent) {
-        if (base == 0 && exponent < 0)
-            throw new ArithmeticException("Cannot raise 0 to a negative power.");
-        return Math.pow(base, exponent);
-    }
-
-    public static double squareRoot(double num) {
-        if (num < 0)
-            throw new IllegalArgumentException("Cannot take square root of negative number.");
-        return Math.sqrt(num);
-    }
-
-    public static boolean isPrime(int num) {
-        if (num <= 1)
-            return false;
-        for (int i = 2; i <= Math.sqrt(num); i++)
-            if (num % i == 0)
-                return false;
-        return true;
-    }
-
-    public static long factorial(int n) {
-        if (n < 0)
-            throw new IllegalArgumentException("Factorial is undefined for negative numbers.");
-        long result = 1;
-        for (int i = 2; i <= n; i++)
-            result *= i;
-        return result;
-    }
     
     // Test case
     public static void main(String[] args) {
+    	AdvancedCalculator
     	
 		// Testing the power method for errors. Found 1
     	try {
@@ -76,7 +47,6 @@ public class Testclass {
 
     	} catch (Exception invalidFactorial){
     		System.out.println("error\n");
-    	}
-    	
+    	}	
     }
 }
