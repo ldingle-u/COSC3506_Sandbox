b02c5b11d5fefaced2668f57aff0ba3a3a8727d2
SofiaOlea-Algoma <soleayerena@algomau.ca>
2026-04-02 15:41:47 -0400

    Wrote tests and fixed issues

--- AdvancedCalculator.java
+++ AdvancedCalculator.java
@@ -3,7 +3,24 @@ public class AdvancedCalculator {
 
     // Calculates the power of a number
     public double power(double base, int exponent) {
-        if (exponent < 0) {
+
+        /*
+        Issue found: 0^-1 had a result of infinity by doing 1.0/0.0
+
+        Now when the base is 0, the result is 0 by default
+
+        I also added an if statement that returns 1 of the exponent is 0
+         */
+
+        if (exponent == 0)
+        {
+            return 1;
+        }
+        else if (base == 0)
+        {
+            return 0;
+        }
+        else if (exponent < 0) {
             return 1 / Math.pow(base, -exponent);
         }
         // Return a double, represents base raised to the exponent
@@ -23,7 +40,14 @@ public class AdvancedCalculator {
         if (number <= 1) {
             return false;
         }
-        for (int i = 2; i < Math.sqrt(number); i++) {
+        /*
+        Issue found: Numbers that aren't divisible by 2 return the wrong answer
+
+        The = sign was missing in the i <= Math.sqrt(number) statement
+
+        Tricky...
+         */
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 return false;
             }
@@ -35,6 +59,16 @@ public class AdvancedCalculator {
     public int factorial(int number) {
         if (number < 0) {
             throw new IllegalArgumentException("Factorial is undefined for negative numbers");
+        }
+
+        /*
+        Issue found: Numbers greater than 12 exceed the capacity of an int
+
+        Now when the input number is greater than 12, it throws an exception
+         */
+        else if (number > 12)
+        {
+            throw new IllegalArgumentException("Factorial is too big for an int type");
         }
         int result = 1;
         // Iteratively calculates factorial in an ascending fashion
--- AdvancedCalculatorTest.java
+++ AdvancedCalculatorTest.java
@@ -7,18 +7,63 @@ class AdvancedCalculatorTest
     {
         AdvancedCalculator calculator = new AdvancedCalculator();
 
-        double result = calculator.power(2, 3);
-        assertEquals(8, result);
+        assertEquals(8, calculator.power(2, 3));
 
-        double result2 = calculator.power(2, -3);
         double division = 1.0/8.0;
-        assertEquals(division, result2);
+        assertEquals(division, calculator.power(2, -3));
 
-        double result3 = calculator.power(0, -1);
-        assertEquals(0, result3);
+        assertEquals(0, calculator.power(0, -1));
 
-
+        //Extra test
+        assertEquals(1, calculator.power(879247, 0));
     }
 
+    @Test
+    public void squareRootTest()
+    {
+        AdvancedCalculator calculator = new AdvancedCalculator();
 
+        assertEquals(3, calculator.squareRoot(9));
+
+        assertThrows(IllegalArgumentException.class,
+                ()-> calculator.squareRoot(-4));
+
+        //Extra test
+        assertEquals(0, calculator.squareRoot(0));
+    }
+
+    @Test
+    public void isPrimeTest()
+    {
+        AdvancedCalculator calculator = new AdvancedCalculator();
+
+        assertEquals(true, calculator.isPrime(13));
+        assertEquals(false, calculator.isPrime(10));
+        assertEquals(true, calculator.isPrime(29));
+        assertEquals(false, calculator.isPrime(1));
+
+        //Extra test
+        assertEquals(false, calculator.isPrime(9));
+    }
+
+    @Test
+    public void factorialTest()
+    {
+        AdvancedCalculator calculator = new AdvancedCalculator();
+
+        assertEquals(120, calculator.factorial(5));
+
+        assertEquals(1, calculator.factorial(0));
+
+        assertThrows(IllegalArgumentException.class,
+                ()-> calculator.factorial(-3));
+
+        assertThrows(IllegalArgumentException.class,
+                ()-> calculator.factorial(20));
+
+        //Extra tests
+        assertEquals(479001600, calculator.factorial(12));
+        assertThrows(IllegalArgumentException.class,
+                ()-> calculator.factorial(13));
+    }
 }
