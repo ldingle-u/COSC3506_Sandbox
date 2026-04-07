4220215477d18c441fbe3cf301f405a3ac4da56a
321assiRanoM <mrpregillana@gmail.com>
2026-04-07 13:46:54 -0400

    Fix prime number check loop condition

--- AdvancedCalculator.java
+++ AdvancedCalculator.java
@@ -23,7 +23,7 @@ public class AdvancedCalculator {
         if (number <= 1) {
             return false;
         }
-        for (int i = 2; i < Math.sqrt(number); i++) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 return false;
             }
